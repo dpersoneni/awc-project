e48cb9591b2c416ee26ffbda8513fd5b6c8fd637
danilo <danilo@danilo.station>
2020-05-20 09:24:02 +0200

    test json

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-var mydata = JSON.parse(prodotti);
-var paragraph = document.getElementById("div1");
-for ( i=0; i < mydata.length; i++) {
-
-    paragraph.innerHTML += "<p>" + mydata[i].name + "</p>"
+obj = { table: "customers", limit: 20 };
+dbParam = JSON.stringify(obj);
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    myObj = JSON.parse(this.responseText);
+    txt += "<table border='1'>"
+    for (x in myObj) {
+      txt += "<tr><td>" + myObj[x].name + "</td></tr>";
+    }
+    txt += "</table>"
+    document.getElementById("demo").innerHTML = txt;
+  }
 }
+xmlhttp.open("POST", "json_demo_db_post.php", true);
+xmlhttp.setRequestHeader("Content-type", "https://github.com/dpersoneni/awc-project/blob/master/prodotti.json");
+xmlhttp.send("x=" + dbParam); 
