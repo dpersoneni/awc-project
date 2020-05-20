1233b298737593cf43793c1b16ae6419adfd529b
danilo <danilo@danilo.station>
2020-05-20 14:11:57 +0200

    json risolto

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-obj = { table: "customers", limit: 20 };
-dbParam = JSON.stringify(obj);
-xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    myObj = JSON.parse(this.responseText);
-    txt += "<table border='1'>"
-    for (x in myObj) {
-      txt += "<tr><td>" + myObj[x].name + "</td></tr>";
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var jsonObject = JSON.parse(this.responseText);
+        txt = "";
+        txt += "<table border='1'>"
+        for (x in jsonObject) {
+
+            txt += "<tr><td>" + jsonObject[x].name + "</td></tr>";
+        }
+        txt += "</table>"
+        document.getElementById("ciao").innerHTML = txt;
+
     }
-    txt += "</table>"
-    document.getElementById("demo").innerHTML = txt;
-  }
 }
-xmlhttp.open("POST", "json_demo_db_post.php", true);
-xmlhttp.setRequestHeader("Content-type", "https://github.com/dpersoneni/awc-project/blob/master/prodotti.json");
-xmlhttp.send("x=" + dbParam); 
+xhr.open('GET', 'https://raw.githubusercontent.com/dpersoneni/awc-project/master/prodotti1.json', true);
+xhr.send(null);
