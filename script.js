a7d38e08efd299f5c0f712704ec2a67aa6f64829
danilo <personeni.dd@gmail.com>
2020-06-03 09:44:33 +0200

    changed plates

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var jsonObject = JSON.parse(xhr.responseText);
+        
         txt = "";
         txt += "<table border='1'>"
         for (x in jsonObject) {
@@ -11,6 +12,17 @@ xhr.onreadystatechange = function() {
         }
         txt += "</table>"
         document.getElementById("ciao").innerHTML = txt;
+
+        txt1 = "";
+        txt1 += "<table border='1'>"
+        x = 3;
+        for (i in jsonObject[x].ingredients) {
+                // txt1 += "<tr><td>" + jsonObject[x].ingredients[i] + "</td></tr>";
+                console.log(jsonObject[x].ingredients[i] )
+        }
+
+        txt1 += "</table>"
+        document.getElementById("ingredienti").innerHTML = txt1;
 
     }
 }
