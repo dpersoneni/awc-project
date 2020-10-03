206719dd24cd0acc3b3217d7da29b1c9e62c10b7
danilo <personeni.dd@gmail.com>
2020-10-03 11:27:31 +0200

    vsijvnreiu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -24,7 +25,7 @@ xhr.onreadystatechange = function() {
 }
 xhr.open('GET', 'https://raw.githubusercontent.com/dpersoneni/awc-project/master/prodotti1.json', true);
 xhr.send(null);
-
+ 
 
 function filtra() {
     
@@ -32,11 +33,10 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var jsonObject = JSON.parse(xhr.responseText);
-        
         txt = "";
         txt += "<div class='row'>"
-        for (x in jsonObject) {
-            if (jsonObject[x].type == document.getElementById("type").value) {
+        for (x in jsonObject) {           
+            if (jsonObject[x].type == document.getElementById("type").value &&  jsonObject[x].price <= parseInt(document.getElementById("price").value)  ) {
             txt += " <div class='flip-card'> <div class='flip-card-inner'> <div class='flip-card-front'>"
             txt += "<img src='images/Amburger.jpg' style='width:100%'>";
             txt += "<p style='float: left; display: inline; margin-left: 10px; margin-top: 3px;'>"+jsonObject[x].name + "</p>"
