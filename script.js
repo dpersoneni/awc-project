a41ebad2a6fb98985688136388588441b1c84208
AmritaLal <amrita.lal@studenti.unimi.it>
2020-10-13 17:04:54 +0200

    ciao

--- script.js
+++ script.js
@@ -1,10 +1,143 @@
+prodotti =  
+[
+    {
+        "id" : 1,   
+        "name":"AmBurger",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 8.00,
+        "ingredients" : [
+            "bread", 
+            "hamburger",
+            "ketchup",
+            "onions",
+            "mustard",
+            "pickle"
+        ]
+    },
+    {
+        "id" : 2,   
+        "name":"AmBurDen",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 7.50,
+        "ingredients" : [
+            "bread", 
+            "chicken",
+            "caesar sauce"
+     ]
+    },
+    {
+        "id" : 3,
+        "name":"Ciobber",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 6.50,
+        "ingredients" : [
+            "bread", 
+            "hamburger",
+            "ketchup",
+            "onions",
+            "mustard",
+            "cheese"
+        ]
+    },
+    {
+        "id": 4,
+        "name":"Veggie",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 8.00,
+        "ingredients" : [
+            "bread", 
+            "soy burger",
+            "tomato",
+            "onions",
+            "mayonnaise",
+            "pickle",
+            "lattuce"
+        ]
+    },
+    {
+        "id" : 5,
+        "name":"McBEQon",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 5.70,
+        "ingredients" : [
+            "bread", 
+            "hamburger",
+            "crispy sauce",
+            "bacon",
+            "cheese"
+        ]
+    },
+    {
+        "id" : 6,
+        "name":"AriPizza",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pizza",
+        "price": 6.50,
+        "ingredients" : [
+            "dough", 
+            "tomato sauce",
+            "mozzarella",
+            "oregano"
+            
+        ]
+    },
+    {
+        "id": 7,
+        "name":"PizzAm",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pizza",
+        "price": 7.00,
+        "ingredients" : [
+            "dough", 
+            "tomato sauce",
+            "mozzarella",
+            "zucchini",
+            "egg plant"
+            
+            
+        ]
+    },
+    {
+        "id":8,
+        "name":"AmAtriciana",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pasta",
+        "price": 8.00,
+        "ingredients" : [
+            "spaghetti", 
+            "guanciale",
+            "pecorino",
+            "tomatoes"  
+        ]
+    },
+    {
+        "id":9,
+        "name":"Troie al Pesto",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pasta",
+        "price": 7.00,
+        "ingredients" : [
+            "trofie", 
+            "pecorino",
+            "basil pesto"
+            
+            
+        ]
+    }
+ ]
+
+localStorage.setItem('prodotti', JSON.stringify(prodotti));
+var jsonObject = JSON.parse(localStorage.getItem('prodotti')) ;
+
+console.log(jsonObject);
 
 function mostraProdotti() {
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var jsonObject = JSON.parse(xhr.responseText);
-        
+
         txt = "";
         txt += "<div class='row'>"
         for (x in jsonObject) {
@@ -20,11 +153,9 @@ xhr.onreadystatechange = function() {
         }
         txt += "</div>"
         document.getElementById("ciao").innerHTML = txt;
-    }
+    
 }
-xhr.open('GET', 'https://raw.githubusercontent.com/dpersoneni/awc-project/master/prodotti1.json', true);
-xhr.send(null);
-}
+
 
 function filtraTipo() {
 var xhr = new XMLHttpRequest();
---
+++ try.js
@@ -0,0 +1,134 @@
+prodotti =  
+[
+    {
+        "id" : 1,   
+        "name":"AmBurger",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 8.00,
+        "ingredients" : [
+            "bread", 
+            "hamburger",
+            "ketchup",
+            "onions",
+            "mustard",
+            "pickle"
+        ]
+    },
+    {
+        "id" : 2,   
+        "name":"AmBurDen",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 7.50,
+        "ingredients" : [
+            "bread", 
+            "chicken",
+            "caesar sauce"
+     ]
+    },
+    {
+        "id" : 3,
+        "name":"Ciobber",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 6.50,
+        "ingredients" : [
+            "bread", 
+            "hamburger",
+            "ketchup",
+            "onions",
+            "mustard",
+            "cheese"
+        ]
+    },
+    {
+        "id": 4,
+        "name":"Veggie",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 8.00,
+        "ingredients" : [
+            "bread", 
+            "soy burger",
+            "tomato",
+            "onions",
+            "mayonnaise",
+            "pickle",
+            "lattuce"
+        ]
+    },
+    {
+        "id" : 5,
+        "name":"McBEQon",
+        "image":"INSERISCI IMMAGINE",
+        "type":"panini",
+        "price": 5.70,
+        "ingredients" : [
+            "bread", 
+            "hamburger",
+            "crispy sauce",
+            "bacon",
+            "cheese"
+        ]
+    },
+    {
+        "id" : 6,
+        "name":"AriPizza",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pizza",
+        "price": 6.50,
+        "ingredients" : [
+            "dough", 
+            "tomato sauce",
+            "mozzarella",
+            "oregano"
+            
+        ]
+    },
+    {
+        "id": 7,
+        "name":"PizzAm",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pizza",
+        "price": 7.00,
+        "ingredients" : [
+            "dough", 
+            "tomato sauce",
+            "mozzarella",
+            "zucchini",
+            "egg plant"
+            
+            
+        ]
+    },
+    {
+        "id":8,
+        "name":"AmAtriciana",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pasta",
+        "price": 8.00,
+        "ingredients" : [
+            "spaghetti", 
+            "guanciale",
+            "pecorino",
+            "tomatoes"  
+        ]
+    },
+    {
+        "id":9,
+        "name":"Troie al Pesto",
+        "image":"INSERISCI IMMAGINE",
+        "type":"pasta",
+        "price": 7.00,
+        "ingredients" : [
+            "trofie", 
+            "pecorino",
+            "basil pesto"
+            
+            
+        ]
+    }
+ ]
+
+
