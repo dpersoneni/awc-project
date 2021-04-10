67e7892c7695742f51c814171d78b69f56621f37
AmritaLal <amrita.lal@studenti.unimi.it>
2021-04-10 12:21:31 +0200

    ciao

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ function mostraProdotti() {
         for (i in jsonObject[x].ingredients) {
             txt += "<li>" + jsonObject[x].ingredients[i] + "</li>"
         }
-        txt += "</ul>  <button type='button' onclick='aggiungiCarrello(" + x + ")'>Aggiungi al carrello</button></div> </div> </div>"
+        txt += "</ul>  <button type='button' onclick='aggiungiCarrello(" + x + ")'>Aggiungi al carrello</button>"
+        txt += "</ul>  <button  type='button' style='width: 2em' onclick='rimuoviCarrello(" + x + ")' > - </button></div> </div> </div>"
     }
 
     txt += "</div>"
     document.getElementById("piatti").innerHTML = txt;
 
-    if ("carrello" in sessionStorage) { 
+    if ("carrello" in sessionStorage) {
         sessionStorage.clear();
         riempiCarrello();
     }
@@ -33,16 +34,18 @@ function mostraProdotti() {
 function riempiCarrello() { // funzione che parte con quantità e totali uguale a zero e mette nel carrello i prodotti che scegliamo 
     qnt = 0;
     tot = 0;
-    txt = "<span><table><tr><th>Prodotto</th><th>Prezzo</th><th>Quantità</th></tr>";
+    txt = "<span style= 'min-width: 20em'><table style= 'min-width: 20em'><tr><th>Prodotto</th><th>Prezzo</th><th>Quantità</th></tr>";
     carrello = JSON.parse(sessionStorage.getItem("carrello"));
     for (c in carrello) {
 
         qnt += carrello[c].qnt;
         tot += carrello[c].price * carrello[c].qnt;
-        txt += "<tr><td>" + carrello[c].name + "</td><td> " + carrello[c].price + "</td><td> " + carrello[c].qnt + "</td></tr>\n";
-
+        if (carrello[c].qnt > 0) {
+            txt += "<tr><td>" + carrello[c].name + "</td><td> " + carrello[c].price + "</td><td> " + carrello[c].qnt + "</td></tr>\n";
+        }
+      
     }
-    document.getElementById("carrello").innerHTML = txt + "</table></span> \n " + "Prezzo totale" + "   " + tot + "<br><button type='button' onclick=window.location.href='logutente.html'>Procedi all'acquisto</button>";
+    document.getElementById("carrello").innerHTML = txt + "</table></span> \n " + "<br><p style='float: right'><b>Prezzo totale</b>" + "   " + tot + "</p><br><br><button type='button' style='float: right' onclick=window.location.href='logutente.html'>Procedi all'acquisto</button>";
     document.getElementById("numeroelementi").innerHTML = qnt;
 }
 
@@ -61,7 +64,7 @@ function aggiungiCarrello(x) {
     };
 
     var index = carrello.findIndex(function (item, i) { // verifica che non ci siano elementi uguali 
-        return item.id == obj.id; 
+        return item.id == obj.id;
     });
 
     if (index === -1) { // se dovessimo avere solo una card, che non si ripete allora nel carrello pushiamo obj
@@ -69,6 +72,38 @@ function aggiungiCarrello(x) {
     } else {
         carrello[index].qnt++;  // altrimenti aggiugiamo solo la quantità di quante volte quel prodotto sia stato scelto
     }
+
+    sessionStorage.setItem('carrello', JSON.stringify(carrello)); // aggiugiamo quindi nel local il carrello con i prodotti scelti 
+
+    riempiCarrello();  // funzione che aggiunge i prodotti nella tabella con "nome, prezzo e quantità"
+}
+
+// cambiato da qui
+function rimuoviCarrello(x) {
+    var carrello = [];
+    if ("carrello" in sessionStorage) {
+        carrello = JSON.parse(sessionStorage.getItem('carrello'));
+    }
+
+    obj = {
+        "id": jsonObject[x].id,
+        "name": jsonObject[x].name,
+        "price": jsonObject[x].price,
+        "qnt": 1
+    };
+
+    var index = carrello.findIndex(function (item, i) { // verifica che non ci siano elementi uguali 
+        return item.id == obj.id;
+    });
+
+    if (index === -1) { // se dovessimo avere solo una card, che non si ripete allora nel carrello pushiamo obj
+        carrello.push(obj);
+    } else {
+        if (carrello[index].qnt > 0) {
+            carrello[index].qnt--; 
+        }
+    }
+        
 
     sessionStorage.setItem('carrello', JSON.stringify(carrello)); // aggiugiamo quindi nel local il carrello con i prodotti scelti 
 
